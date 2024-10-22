a4bd492eabbda76ce2ec6c4375a800323c83255e
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2024-10-22 07:02:25 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "default_subnet" {
+  default = "e2lkftfqgdeino13v9h0"
+}
+
+variable "default_image" {
+  default = "fd8icq49od5bhtnpcvnj"
+}
+
+variable "default_disk_size" {
+  default = "20"
+}
+
+variable "cloud_init_file" {
+  default = "./config.txt"
+}
